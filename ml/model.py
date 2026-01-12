22b1ab82e832b2d545befa195af97c25f1a87649
Noah <n.schouame@gmail.com>
2026-01-12 17:03:48 +0100

    end MHA

--- ml/model.py
+++ ml/model.py
@@ -19,10 +19,10 @@ with open("config.yml", "r") as config_file:
 @dataclass 
 class ModelConfig: 
     vocab_size: int = config["model"]["config"]["vocab_size"]
-    dim_embeddings: int = config["model"]["config"]["dim_embeddings"]
-    nb_heads: int = config["model"]["config"]["nb_heads"]
-    nb_layers: int = config["model"]["config"]["nb_layers"]
-    dim_ff: int = config["model"]["config"]["dim_ff"]
+    d_model: int = config["model"]["config"]["dim_embeddings"]
+    n_heads: int = config["model"]["config"]["nb_heads"]
+    n_layers: int = config["model"]["config"]["nb_layers"]
+    d_ff: int = config["model"]["config"]["dim_ff"]
     max_seq_len: int = config["model"]["config"]["max_seq_len"]
     dropout: float = config["model"]["config"]["dropout"]
 
@@ -31,10 +31,51 @@ class ModelConfig:
 class MultiHeadAttention(nn.Module):
     def __init__(self, config: ModelConfig):
         super().__init__()
-        assert config.dim_embeddings % config.nb_heads == 0 
+        assert config.d_model % config.n_heads == 0 
 
-        self.dim_embeddings = config.dim_embeddings
-        self.nb_heads = config.nb_heads
-        self.dim_k = config.dim_embeddings // config.nb_heads
+        # Generals
+        self.d_model = config.d_model
+        self.n_heads = config.n_heads
+        self.d_k = config.d_model // config.n_heads
 
+        # Q, K, V
+        self.W_q = nn.Linerar(config.d_model, config.d_model)
+        self.W_k = nn.Linerar(config.d_model, config.d_model)
+        self.W_v = nn.Linerar(config.d_model, config.d_model)
+
+        # Output proj
+        self.W_o = nn.Linerar(config.d_model, config.d_model)
+
+        # Dropout 
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x, mask=None): 
+        batch_size, seq_len, d_model = x.shape
+
+        # Proj + multihead reshape
+        Q = self.W_q.view(batch_size, seq_len, self.n_heads, self.d_k).transpose(1, 2)
+        K = self.W_k.view(batch_size, seq_len, self.n_heads, self.d_k).transpose(1, 2)
+        V = self.W_v.view(batch_size, seq_len, self.n_heads, self.d_k).transpose(1, 2)
+
+        # Attention scores
+        scores = torch.matmul(Q, K.transpose(-2, -1) / math.sqrt(self.d_k))
+
+        # Causal attention mask
+        if mask is not None: 
+            scores = scores.masked_fill(mask == 0, float('-inf'))
         
+        # Softmax
+        attention_weights = F.softmax(scores, dim=-1)
+
+        # Dropout
+        attention_weights = self.dropout(attention_weights)
+
+        # Attention to values
+        output = torch.matmul(attention_weights, V)
+
+        # Reshape + final proj
+        output = output.transpose(1, 2).contiguous().view(batch_size, seq_len, d_model)
+        output = self.W_o(output)
+
+        # Return 
+        return output
